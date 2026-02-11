2db9f6221eff256e18c192f7b9c5d50a72364dfd
marija <marijasimic1422@gmail.com>
2026-02-11 19:24:07 +0100

    Add logo image and update page styles and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,58 +7,23 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src="/LOGO LEXORIA.png"
+          alt="Lexoria logo"
+          width={300}
+          height={100}
           priority
+          style={{ objectFit: "contain" }}
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1>Uskoro ćemo biti dostupni</h1>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+            U međuvremenu nas možete kontaktirati putem emaila:{" "}
+            <a href="mailto:info@lexoria.rs">info@lexoria.rs</a>
           </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <p>
+            ili telefona:{" "}
+            <a href="tel:+381653731935">+381653731935</a>
+          </p>
         </div>
       </main>
     </div>
